84ec4ca063da6fec58cbbdfe08bcb32c70fdded2
BrandonsCode <brandonscpucode@gmail.com>
2022-05-22 08:12:16 -0400

    added  secret key to setting

--- btre/settings.py
+++ btre/settings.py
@@ -24,6 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
+SECRET_KEY='django-insecure-esio2nrtv#tu0bacbb=(9dxd57&3a81kkhga)g0qdyv^@tqq3)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
