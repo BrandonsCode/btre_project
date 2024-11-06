2ad76d9f6babe88e57e43338f1ec6eb0036abf37
BrandonsCode <brandonscpucode@gmail.com>
2024-11-06 04:15:17 -0500

    change gitingore

--- btre/settings.py
+++ btre/settings.py
@@ -76,15 +76,8 @@ WSGI_APPLICATION = 'btre.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': '123456',
-        'HOST': 'localhost'
-    }
-}
+
+
 
 
 # Password validation
